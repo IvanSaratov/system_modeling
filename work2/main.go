cff757771154165da11b9dcea19a3b3ade6681b7
unknown <D:\BAT MAILS>
2024-03-21 11:03:24 +0400

    try new

--- work2/main.go
+++ work2/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"gonum.org/v1/gonum/mat"
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/plotutil"
@@ -11,24 +10,32 @@ import (
 // Будем использовать метод Рунге-Кутты
 // Введем дополнительные переменные u1 = x, u2 = x', v1 = y, v2 = y'
 
-func rungeKutta(u1, u2, v1, v2, t, h float64) (float64, float64, float64, float64) {
+// func rungeKutta(u1, u2, v1, v2, t, h float64) (float64, float64, float64, float64) {
 
-	k1 := mat.NewVecDense(4, []float64{u2, v2, 0, 0})
-	k2 := mat.NewVecDense(4, []float64{-5*u1 + 2*v1, 0, 0, 0})
-	k3 := mat.NewVecDense(4, []float64{v2, 0, u2, 0})
-	k4 := mat.NewVecDense(4, []float64{2*u1 - 8*v1, 0, 0, u2})
+// 	k1 := mat.NewVecDense(4, []float64{u2, v2, 0, 0})
+// 	k2 := mat.NewVecDense(4, []float64{-5*u1 + 2*v1, 0, 0, 0})
+// 	k3 := mat.NewVecDense(4, []float64{v2, 0, u2, 0})
+// 	k4 := mat.NewVecDense(4, []float64{2*u1 - 8*v1, 0, 0, u2})
 
-	deltaU1 := (k1.At(0, 0) + 2*k2.At(0, 0) + 2*k3.At(0, 0) + k4.At(0, 0)) * (h / 6)
-	deltaU2 := (k1.At(1, 0) + 2*k2.At(1, 0) + 2*k3.At(1, 0) + k4.At(1, 0)) * (h / 6)
-	deltaV1 := (k1.At(2, 0) + 2*k2.At(2, 0) + 2*k3.At(2, 0) + k4.At(2, 0)) * (h / 6)
-	deltaV2 := (k1.At(3, 0) + 2*k2.At(3, 0) + 2*k3.At(3, 0) + k4.At(3, 0)) * (h / 6)
+// 	deltaU1 := (k1.At(0, 0) + 2*k2.At(0, 0) + 2*k3.At(0, 0) + k4.At(0, 0)) * (h / 6)
+// 	deltaU2 := (k1.At(1, 0) + 2*k2.At(1, 0) + 2*k3.At(1, 0) + k4.At(1, 0)) * (h / 6)
+// 	deltaV1 := (k1.At(2, 0) + 2*k2.At(2, 0) + 2*k3.At(2, 0) + k4.At(2, 0)) * (h / 6)
+// 	deltaV2 := (k1.At(3, 0) + 2*k2.At(3, 0) + 2*k3.At(3, 0) + k4.At(3, 0)) * (h / 6)
 
-	u1 += deltaU1
-	u2 += deltaU2
-	v1 += deltaV1
-	v2 += deltaV2
+// 	u1 += deltaU1
+// 	u2 += deltaU2
+// 	v1 += deltaV1
+// 	v2 += deltaV2
 
-	return u1, u2, v1, v2
+// 	return u1, u2, v1, v2
+// }
+
+func dxdt(x, y float64) float64 {
+	return (-5 * x) + (2 * y)
+}
+
+func dydt(x, y float64) float64 {
+	return (2 * x) - (8 * y)
 }
 
 func main() {
@@ -44,22 +51,17 @@ func main() {
 	points := make(plotter.XYs, 0)
 
 	// Задаем начальные условия
-	u1 := 1.0
-	u2 := 0.0
-	v1 := 1.0
-	v2 := 0.0
-	t := 0.0     // Начальное времея
-	h := 0.1     // Шаг
-	tmax := 10.0 // Максимальное время
-
-	steps := int(tmax / h)
+	x := 1.0
+	y := 1.0
+	h := 0.01     // Шаг
+	tmax := 100.0 // Максимальное время
 
 	// Вычисляем значения точек
-	for i := 0; i < steps; i++ {
-		u1, u2, v1, v2 = rungeKutta(u1, u2, v1, v2, t, h)
-		t += h
+	for t := 0.0; t < tmax; t += h {
+		x = x + h*dxdt(x, y)
+		y = y + h*dydt(x, y)
 
-		points = append(points, plotter.XY{X: u1, Y: v1})
+		points = append(points, plotter.XY{X: x, Y: y})
 	}
 
 	// Создаем график с точками
